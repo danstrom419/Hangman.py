f5ac2274423438f304f1015f63bf6b0d4fc24974
danstrom419 <dll9@sunyorange.edu>
2025-04-06 23:19:16 -0400

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,32 +1,36 @@
 from image import *
 def play(word):
-    # prompt user
+    # initializes our variables and lists
     incorrect_guesses = 0
     secret_word = list(word)
+    # this is to keep track of every letter guesses
     guessed_letters = set()
+    # this is to keep track of each unique letter in the random word generated
     unique_letters = set(secret_word)
     print(hangman_image[0])
+    # lets the user know the length of the word
     print(len(word)*" _ ")
-    # loop - counter controlled - GLOBAL
+    # loop that continues until 6 incorrect guesses occur
     while incorrect_guesses < 6:
       user_input = input("\nGuess a letter in the word: ").lower()
+      # makes it so the user input can only be a letter
       if user_input.isalpha() is False:
           print("Only letters can be guessed.")
-      
+      # checks if the user input has already been guessed in the guessed-letters set
       elif user_input in guessed_letters:
           print("Letter has already been guessed!")
           print(f"Letters guessed: {sorted(guessed_letters)}")
-      
+      # checks if the user input is in the secret word
       elif user_input not in secret_word:
         incorrect_guesses += 1
         guessed_letters.add(user_input)
         print(f"Letters guessed: {sorted(guessed_letters)}")
-      
+      # checks if the user input is in the secret word
       elif user_input in secret_word:
         print("Your guess is correct")
         guessed_letters.add(user_input)
         print(f"Letters guessed: {sorted(guessed_letters)}")
-      
+      # displays the word guessing porgress and  updates it if any guesses are correct by looping though the letters in teh secret word and seeing if they are in guessed_letters set
       display = []
       for x in secret_word:
         if x in guessed_letters:
@@ -34,7 +38,7 @@ def play(word):
         else:
             display.append("_")
       print(' '.join(display))
-
+      # displays the hangman status by pulling from the list on the image.py file
       if incorrect_guesses == 0:
         print(hangman_image[0])
       elif incorrect_guesses == 1:
@@ -49,7 +53,7 @@ def play(word):
         print(hangman_image[5])
       elif incorrect_guesses == 6:
         print(hangman_image[6])
-
+       # win condition, checks if the unique_letters set is a subset within guessed_letters if so you win.
       if unique_letters.issubset(guessed_letters):
         print(f"Congratulations you win! The word is {word}!")
         break
